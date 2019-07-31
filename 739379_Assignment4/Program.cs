7292c51cf3fbf08b74342df75545dec2fec7eb7b
Sandeep <ssd2192@gmail.com>
2019-07-31 08:29:05 -0400

    A-A

---
+++ 739379_Assignment4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _739379_Assignment4
+{
+    class Program
+    {
+        static string name;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tell me your Name");
+            name = Console.ReadLine();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Playground
+    {
+        public void Run()
+        {
+            Dictionary<string, int> Gematria = new Dictionary<string, int>(){
+                {"A",1},
+                {"B", 2},
+                {"C",3}
+            };
+            Console.WriteLine(Gematria["A"]);
+        }
+    }
+}
