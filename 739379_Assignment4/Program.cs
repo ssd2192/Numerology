300ed27453a7040ddd22384e05d372734e645ee9
Sandeep <ssd2192@gmail.com>
2019-07-31 09:21:16 -0400

    A-B

--- 739379_Assignment4/Program.cs
+++ 739379_Assignment4/Program.cs
@@ -8,13 +8,70 @@ namespace _739379_Assignment4
 {
     class Program
     {
-        static string name;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Tell me your Name");
-            name = Console.ReadLine();
+            Gematria g = new Gematria();
+            g.Run();
 
+            Playground p = new Playground();
+            p.Run();
             Console.ReadKey();
+        }
+    }
+
+    class Gematria
+    {
+
+        public static Dictionary<string, int> LetterValue = new Dictionary<string, int>(){
+                {"A",1},
+                {"B", 2},
+                {"C",3}
+            };
+
+        public void Run()
+        {
+            Console.WriteLine("Tell me your Name");
+            string name = Console.ReadLine();
+            this.RunGeomatriaCalculator(name);
+            
+        }
+
+        private void RunGeomatriaCalculator(string name)
+        {
+            // walk over string and pluck
+            foreach (char letter in name)
+            {
+                Console.WriteLine(letter);
+                // int numValue = getletterNumber(letter);
+                this.RunGeomatriaCalculator(letter);
+
+
+
+            }
+
+        }
+
+        private void RunGeomatriaCalculator(char letter)
+        {
+            // walk over string and pluck
+            int temp = LetterValue["B"];
+            Console.WriteLine(temp);
+
+        }
+
+        private int getletterNumber(char letter)
+        {
+            int numValue = 0;
+
+            switch (letter)
+            {
+                case 'A':
+                    numValue = 1;
+                    break;
+            }
+
+            return numValue;
         }
     }
 
@@ -22,12 +79,14 @@ namespace _739379_Assignment4
     {
         public void Run()
         {
-            Dictionary<string, int> Gematria = new Dictionary<string, int>(){
+            Dictionary<string, int> LetterValue = new Dictionary<string, int>(){
                 {"A",1},
                 {"B", 2},
                 {"C",3}
             };
-            Console.WriteLine(Gematria["A"]);
+
+            int temp = LetterValue["B"];
+            Console.WriteLine(LetterValue["A"]);
         }
     }
 }
